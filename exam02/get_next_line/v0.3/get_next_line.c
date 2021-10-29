fe9b2c0414ad50390b6510b6dbf5d304bdb7b8cd
Jesse Van der wolf <jevan-de@f1r1s17.codam.nl>
2021-10-29 14:50:45 +0200

    Newest version of GNL

--- exam02/get_next_line/v0.3/get_next_line.c
+++ exam02/get_next_line/v0.3/get_next_line.c
@@ -1,20 +1,128 @@
 #include "get_next_line.h"
 
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 char    *get_next_line(int fd)
 {
-    static char buff[STATIC_BUFFSIZE];
+    static char *s_buffer = NULL;
+    int         len;
+    ssize_t     ret;
+    char        *internal_buffer;
+    char        *tmp;
     char        *p;
+    int         buffer_size;
+    int         read_count, alloc_count;
 
     /* check if still a line in buff */
-    if ((p = strchr(buff, '\n')))
+    if (s_buffer && (p = strchr(s_buffer, '\n')))
     {
-        
+        internal_buffer = strndup(s_buffer, (p - s_buffer) + 1);
+        tmp = strdup(s_buffer + (p - s_buffer) + 1);
+        if (!tmp || !internal_buffer)
+        {
+            free(internal_buffer);
+            free(tmp);
+            return (NULL);
+        }
+        free(s_buffer);
+        s_buffer = tmp;
+        if (!(*s_buffer))
+        {
+            free(s_buffer);
+            s_buffer = NULL;
+        }
+        return (internal_buffer);
     }
-    return (NULL);
+
+    buffer_size = BUFFER_SIZE;
+    if (buffer_size < 1024)
+        buffer_size = 1024;
+
+    internal_buffer = (char*)malloc(sizeof(char) * (buffer_size + 1));
+    ret = read(fd, internal_buffer, BUFFER_SIZE);
+    if (ret < 0 || (ret == 0 && !s_buffer))
+    {
+        free(internal_buffer);
+        return (NULL);
+    }
+    internal_buffer[ret] = '\0';
+
+    /* read more bytes into buffer so as to find new line */
+    if (ret != 0 && !(p = strchr(internal_buffer, '\n')))
+    {
+        read_count = 1;
+        alloc_count = 0;
+        while (!(p = strchr(internal_buffer, '\n')) && ret != 0)
+        {
+            /* TODO check if buffer needs to be increased */
+            ret = read(fd, internal_buffer + (read_count * BUFFER_SIZE), BUFFER_SIZE);
+            read_count++;
+        }
+        internal_buffer[((read_count - 1) * BUFFER_SIZE) + ret] = '\0';
+    }
+
+    /* if remainder in static buffer copy to internal buffer */
+    if (s_buffer)
+    {
+        len = strlen(s_buffer);
+        if (len > 0)
+        {
+            tmp = (char*)malloc(sizeof(char) * (len + strlen(internal_buffer) + 1));
+            strcpy(tmp, s_buffer);
+            strcpy(tmp + len, internal_buffer);
+            tmp[len + strlen(internal_buffer)] = '\0';
+            free(s_buffer);
+            free(internal_buffer);
+            internal_buffer = tmp;
+            s_buffer = NULL;
+        }
+    }
+
+    /* if remainer in internal buffer copy to static buffer */
+    if ((p = strchr(internal_buffer, '\n')))
+    {
+        if ((long)strlen(internal_buffer) != (p - internal_buffer + 1))
+        {
+            tmp = strdup(internal_buffer + (p - internal_buffer) + 1);
+            s_buffer = tmp;
+            internal_buffer[p - internal_buffer + 1] = '\0';
+        }
+    }
+    return (internal_buffer);
 }
+
+void    leakcheck(void)
+{
+    system("leaks a.out");
+}
+
+/*
+for (( i=1; i < 100; i++ ));
+do
+    if ! diff <(gcc -D BUFFER_SIZE=$i -Wall -Werror -Wextra get_next_line.c && ./a.out) <(cat get_next_line.c);
+    then
+        echo $i;
+    fi;
+done
+*/
 
 int     main(void)
 {
-    get_next_line(0);
-    return (0);
+    char    *line;
+    int     fd;
+
+    //atexit(leakcheck);
+    fd = open("get_next_line.c", O_RDONLY);
+    if (fd < 0)
+        return (EXIT_FAILURE);
+    line = get_next_line(fd);
+    while (line)
+    {
+        printf("%s", line);
+        free(line);
+        line = get_next_line(fd);
+    }
+    return (EXIT_SUCCESS);
 }
--- exam02/get_next_line/v0.3/get_next_line.h
+++ exam02/get_next_line/v0.3/get_next_line.h
@@ -4,10 +4,9 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
 # endif
-# define STATIC_BUFFSIZE 500000000
 
 # include <stdlib.h>
-# include <string.h>
+# include <unistd.h>
 
 char    *get_next_line(int fd);
 
